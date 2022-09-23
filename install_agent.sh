2f7bc607c3cee88890f7fd2dfdac1237a7c8ec9a
Shivamrathi001 <106222099+Shivamrathi001@users.noreply.github.com>
2022-09-23 13:25:06 +0530

    Update install_agent.sh

--- install_agent.sh
+++ install_agent.sh
@@ -4,4 +4,4 @@ sudo apt install wget
 cd /home/ubuntu
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
-sudo ./install auto -y
+sudo ./install auto 
